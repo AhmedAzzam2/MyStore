1e39f18bbbacce2b4e9ac2b320706a5c0f0ec18f
Ahmed <ahmedaz201811@gmail.com>
2022-08-04 23:13:33 +0200

    ProjuctComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CartComponent} from './cart/cart.component';
+import {ConfirmationComponent} from './confirmation/confirmation.component';
+import {ProductItemComponent} from './product-item/product-item.component';
+import {ProductItemDetailComponent} from './product-item-detail/product-item-detail.component';
+import {ProductListComponent} from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cart', component: CartComponent }
+  , { path: 'confirmation', component: ConfirmationComponent }
+  , { path: 'product-item', component: ProductItemComponent }
+  , { path: 'product-item-detail', component: ProductItemDetailComponent }
+  , { path: 'product-list', component: ProductListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
