5b38de626b8a783089c47ee063d9f38651b5d4b8
Ahmed <ahmedaz201811@gmail.com>
2022-08-04 23:59:31 +0200

    app-root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {CartComponent} from './cart/cart.component';
-import {ConfirmationComponent} from './confirmation/confirmation.component';
-import {ProductItemComponent} from './product-item/product-item.component';
-import {ProductItemDetailComponent} from './product-item-detail/product-item-detail.component';
-import {ProductListComponent} from './product-list/product-list.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
+import { CartComponent } from './cart/cart.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+
 
 const routes: Routes = [
-  { path: 'cart', component: CartComponent }
-  , { path: 'confirmation', component: ConfirmationComponent }
-  , { path: 'product-item', component: ProductItemComponent }
-  , { path: 'product-item-detail', component: ProductItemDetailComponent }
-  , { path: 'product-list', component: ProductListComponent }
+  { path: '', component: ProductListComponent },
+  { path: 'product/:id', component: ProductItemDetailComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'confirmation', component: ConfirmationComponent }
+  
 ];
 
 @NgModule({
