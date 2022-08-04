de630edc4a952a4bed1031434c2e532927dd7e3f
Ahmed <ahmedaz201811@gmail.com>
2022-08-04 10:25:33 +0200

    localhost:4200

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { CartComponent } from './cart/cart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductItemDetailComponent,
+    ProductItemComponent,
+    ConfirmationComponent,
+    CartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
