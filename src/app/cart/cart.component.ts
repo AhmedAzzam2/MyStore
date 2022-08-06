05d1b946570a0bd6587f207a7d22cfa5d8621095
Ahmed <ahmedaz201811@gmail.com>
2022-08-06 19:24:58 +0200

    done MyStore

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  projucts: any ;
+  @Input() projucts: any | undefined;
+  @Input() ProductApi: any = JSON.parse(localStorage.getItem("product") || '{}');
+
 
   constructor() { }
 
